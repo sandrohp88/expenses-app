de3729063b88149b648c972d5876c42994213a31
Sandro Herrera Pallares <sandrohp88@gmail.com>
2019-01-29 10:23:33 -0800

    Reformat code to use css modules and sass

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import styles from './App.module.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className={styles.App}>
+        <header className={styles.App_header}>
+          <img src={logo} className={styles.App_logo} alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-            className="App-link"
+            className={styles.App_link}
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
