0807b1a8956e6f164e03d95a1638172b36193cd3
Sandro Herrera Pallares <sandrohp88@gmail.com>
2019-02-08 00:16:24 -0800

    Firebase api

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,23 @@
 import firebase from 'firebase'
 import expenses from '../sampleData/expenses'
 // Initialize Firebase
-const config = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
-}
+// const config = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
+// }
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyASfxshJ8SuhNFrSXPkn6uX_tGdPqfDqIU",
+  authDomain: "expenses-db-6b0ad.firebaseapp.com",
+  databaseURL: "https://expenses-db-6b0ad.firebaseio.com",
+  projectId: "expenses-db-6b0ad",
+  storageBucket: "expenses-db-6b0ad.appspot.com",
+  messagingSenderId: "559328508976"
+};
 firebase.initializeApp(config)
 const expensesRef = firebase
   .firestore()
